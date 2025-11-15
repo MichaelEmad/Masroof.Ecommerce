ac8b51f5a5a575a71a03efd58489a806c68b1ac5
mshafiq.c <mshafiq.c@tetco.sa>
2025-11-15 18:29:23 +0200

    update payment name space

--- src/Masroof.Ecommerce.HttpApi/EcommerceHttpApiModule.cs
+++ src/Masroof.Ecommerce.HttpApi/EcommerceHttpApiModule.cs
@@ -23,7 +23,7 @@ namespace Masroof.Ecommerce;
     typeof(AbpOpenIddictProHttpApiModule),
     typeof(AbpAccountPublicHttpApiModule),
     typeof(AbpFeatureManagementHttpApiModule),
-    typeof(PaymentHttpApiModule)
+    typeof(AbpPaymentHttpApiModule)
     )]
 public class EcommerceHttpApiModule : AbpModule
 {
